881e9c5ea8fe805d5ad4986eb566eef9d71247bb
anton_lazovsky <anton@fksis.by>
2018-03-30 16:48:49 +0300

    solved hamming difference task with python and javascript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -4,4 +4,17 @@
 // You may use ES6 or ES5 to solve.
 
 
+class Hamming {
+    compute(firstStr, secondStr) {
+        if (firstStr.length !== secondStr.length)
+            throw 'DNA strands must be of equal length.';
+        let result = 0;
+        for (let i = 0; i < firstStr.length; i++) {
+            if (firstStr[i] !== secondStr[i])
+                result++;
+        }
+        return result
+    }
+}
+
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,8 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+
+def compute(first_str, second_str):
+    if len(first_str) != len(second_str):
+        raise ValueError('DNA strands must be of equal length.')
+    return sum(first != second for first, second in zip(first_str, second_str))
